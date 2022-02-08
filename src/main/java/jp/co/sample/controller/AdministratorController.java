bc93160d10b4134722d8269f5a44dba6e3307c2c
hayata hashimoto <hayata.hashimoto@rakus-partners.co.jp>
2022-02-08 17:04:41 +0900

    管理者情報を登録する機能の作成。AdministractorController

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,10 +1,14 @@
 package jp.co.sample.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdministratorForm;
 import jp.co.sample.service.AdministratorService;
 
@@ -25,4 +29,17 @@ public class AdministratorController {
 	public String toInsert() {
 		return "administrator/insert";
 	}
+	
+	@RequestMapping("/insert")
+	public String insert(@Validated InsertAdministratorForm form, 
+			BindingResult result) {
+		if(result.hasErrors()) {
+			return toInsert();
+		}
+		Administrator administrator = new Administrator();
+		BeanUtils.copyProperties(form, administrator);
+		administratorService.insert(administrator);
+		return "redirect:/";
+	}
+	
 }
